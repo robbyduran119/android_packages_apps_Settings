5790f5433d1df7777aa0aeafab7f30b34e5dcd21
storm119 <storm119@gmail.com>
2018-03-18 08:23:03 +0800

    derp: Configurable 0, 90, 180 and 270 degree rotation [2/2]

--- src/com/android/settings/preference/DisplayRotation.java
+++ src/com/android/settings/preference/DisplayRotation.java
@@ -61,7 +61,7 @@ public class DisplayRotation extends SettingsPreferenceFragment implements OnPre
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.EXTENSIONS;
+        return MetricsEvent.DIRTYTWEAKS;
     }
 
     @Override
